48baed5c38cc0180110031c0487b0ddc7e2f8620
ed <ebwibble@hotmail.com>
2018-03-13 00:35:11 +0000

    hdl wip

--- hdl/src/rc2014_interface_top.v
+++ hdl/src/rc2014_interface_top.v
@@ -53,11 +53,11 @@ reg [7:0] data_out;
 // );
 
 reg [15:0] lastknownaddress = 0;
-
+reg [7:0] lastknownvalue = 0;
 // !WR &&  && a[7:0] == 8'hC0
 always @(posedge CLK)
 begin
-    ce = (A > 16'hFFFF) && !MRQ && !WR;
+    ce = (A == 16'hffff) && !MRQ && !WR;        
 
     if ((ce || !BUT1) && led_countdown == 0)
     begin
@@ -70,7 +70,8 @@ begin
 
 	if (ce)
 	begin
-		lastknownaddress <= D;
+		lastknownaddress <= A;
+		lastknownvalue <= D;
 	end
     // DATA_DIR = (!MRQ && A >= 16'h8000) && WR && !RD ? 1 : 0;
 
@@ -82,7 +83,7 @@ begin
     // 0 = input, 1 = output
     // DATA_DIR = ce && !MRQ && WR && !RD ? 1 : 0;
     DATA_DIR = 0; //ce;
-    EX7 = 0; //!ce;
+    EX7 = 1; //!ce;
 end
 // 0 when !WR && RD, 1 when WR && !RD
 assign LED1 = led_countdown > 0;
@@ -107,14 +108,14 @@ assign SIG2_DIR = 0;
 	end
 
 
-	reg [7:0] 	text[0:7];
-	reg [3:0]	text_cntr;
+	reg [7:0] 	text[0:12];
+	reg [16:0]	text_cntr;
 
 	reg 		tx_req;
 	reg [7:0]	tx_data;
 	wire		tx_ready;
 
-	reg [7:0] asciidigits [0:3];
+	reg [7:0] asciidigits [0:5];
 
 	// stringy str_inst(.clk(clk), .inputnumber(A[3:0]), .ascii(asciidigits[0]));
 	// stringy str2(.clk(clk), .inputnumber(A[7:4]), .ascii(asciidigits[1]));
@@ -129,8 +130,13 @@ assign SIG2_DIR = 0;
 			text[3]  <= "_";
 			text[4]  <= "_";
 			text[5]  <= "_";
-			text[6] <= "\n";
-			text[7] <= 'h0d;
+			text[6]  <= " ";
+			text[7]  <= "0";
+			text[8]  <= "x";
+			text[9]  <= "_";
+			text[10]  <= "_";
+			text[11] <= "\n";
+			text[12] <= 'h0d;
 			
 			text_cntr	<= 0;
 			tx_req		<= 1'b0;
@@ -148,6 +154,9 @@ assign SIG2_DIR = 0;
 			text[3]  <= asciidigits[2];
 			text[4]  <= asciidigits[1];
 			text[5]  <= asciidigits[0];
+
+			text[9]  <= asciidigits[5];
+			text[10]  <= asciidigits[4];
 
 			tx_req		<= 1'b0;
 
@@ -234,6 +243,47 @@ assign SIG2_DIR = 0;
 				endcase
 
 
+				case (lastknownvalue[3:0])
+					4'h0: asciidigits[4] <= "0";
+					4'h1: asciidigits[4] <= "1";
+					4'h2: asciidigits[4] <= "2";
+					4'h3: asciidigits[4] <= "3";
+					4'h4: asciidigits[4] <= "4";
+					4'h5: asciidigits[4] <= "5";
+					4'h6: asciidigits[4] <= "6";
+					4'h7: asciidigits[4] <= "7";
+					4'h8: asciidigits[4] <= "8";
+					4'h9: asciidigits[4] <= "9";
+					4'hA: asciidigits[4] <= "A";
+					4'hB: asciidigits[4] <= "B";
+					4'hC: asciidigits[4] <= "C";
+					4'hD: asciidigits[4] <= "D";
+					4'hE: asciidigits[4] <= "E";
+					4'hF: asciidigits[4] <= "F";
+					default: asciidigits[4] <= "Z";
+				endcase
+
+				case (lastknownvalue[7:4])
+					4'h0: asciidigits[5] <= "0";
+					4'h1: asciidigits[5] <= "1";
+					4'h2: asciidigits[5] <= "2";
+					4'h3: asciidigits[5] <= "3";
+					4'h4: asciidigits[5] <= "4";
+					4'h5: asciidigits[5] <= "5";
+					4'h6: asciidigits[5] <= "6";
+					4'h7: asciidigits[5] <= "7";
+					4'h8: asciidigits[5] <= "8";
+					4'h9: asciidigits[5] <= "9";
+					4'hA: asciidigits[5] <= "A";
+					4'hB: asciidigits[5] <= "B";
+					4'hC: asciidigits[5] <= "C";
+					4'hD: asciidigits[5] <= "D";
+					4'hE: asciidigits[5] <= "E";
+					4'hF: asciidigits[5] <= "F";
+					default: asciidigits[5] <= "Z";
+				endcase
+
+
 				tx_req		<= 1'b1;
 				tx_data		<= text[text_cntr];
 				text_cntr	<= text_cntr + 1;
@@ -244,7 +294,7 @@ assign SIG2_DIR = 0;
 			tx_data		<= text[text_cntr];
 			text_cntr	<= text_cntr + 1;
 
-			if (text_cntr == 7) begin
+			if (text_cntr == 13) begin
 				text_cntr	<= 0;
 			end
 			else begin
