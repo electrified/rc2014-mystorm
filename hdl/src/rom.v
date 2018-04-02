5443acee7efc0a8ffbf82748ced7bf4bded60a53
ed <ed@maidavale.org>
2018-04-02 18:03:47 +0100

    WIP

--- hdl/src/rom.v
+++ hdl/src/rom.v
@@ -1,12 +1,15 @@
+`default_nettype none
+
 module rom(input clk, 
 input ce, 
-input [12:0] addr, // we discard the higher lines so this value will be 0 - 0x2000
+input [15:0] addr,
 output reg [7:0] rdata);
     initial begin
-        $readmemh("src/bas.hex", mem);
+        $readmemh("src/test_programs/test.out", mem);
+        // $readmemh("src/bas.hex", mem);
     end
 
-    reg [7:0] mem [0:'h2000];
+    reg [7:0] mem [0:16'h2000];
     always @(posedge clk) begin
         if (ce)
         begin
--- hdl/src/test_programs/test.asm
+++ hdl/src/test_programs/test.asm
@@ -1,9 +1,7 @@
 ; doke &h8224,&hc000
 ; print usr(0)
-; sigrok-cli --driver saleae-logic16 --config samplerate=25Mhz --channels 0=D0,1=D1,2=D2,3=D3,4=D4,5=D5,6=D6,7=D7,9=RD,10=WR,11=MREQ,13=M1 --samples 1M --output-file z80
-org $C000
+org $0000
   DEFC data_port = $2500
-
 setup:
   ld a, (data_port)
   jp setup
